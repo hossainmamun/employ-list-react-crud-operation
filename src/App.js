906ac03f67636ec462bcbb915ebb8ecb44281bfa
hossainmamun <hossainmamun337@gmailcom>
2022-08-12 21:09:17 +0600

    update commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import EmployerInfo from './Component/EmployerInfo.js';
-import UserInfo from './Component/UserInfo.js';
 
 function App() {
    return (
